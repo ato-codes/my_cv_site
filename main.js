a06b5b3d8c9bb1febc30154d0d1c336ed4db1039
beamlakTadesse01 <thecmboy930@gmail.com>
2022-12-12 08:09:46 -0800

    my cv website v0.1 with no social media links

---
+++ main.js
@@ -0,0 +1,44 @@
+
+
+const technical_skills = document.getElementById('technical_skills')
+const technical_skills_content = technical_skills.childNodes[3].childNodes[1]
+
+
+const technical_skill = () =>{
+
+    const beamlak_skills = {
+        HTML : 4,
+        JavaScript : 4,
+        Linux : 3,
+        CSS : 3,
+        'Web Applications' : 2,
+        'Data structures' : 2,
+        Algorithms : 2
+    }
+    
+    for(let skill in beamlak_skills){
+        const item_bar = document.createElement('div')
+        item_bar.className = 'item bar'
+    
+        const item_title = document.createElement('div')
+        item_title.className = 'item_title'
+        item_title.textContent = skill
+    
+    
+        const bars = document.createElement('div')
+        bars.className = 'bars'
+    
+        for(let i = 0; i <= 4; i++){
+            const rec = document.createElement('div')
+            rec.className = 'rec'
+            if(beamlak_skills[skill]-1 >= i)rec.className = `${rec.className} fill`
+            bars.append(rec)
+        }
+        item_bar.append(item_title,bars)
+    
+    technical_skills_content.append(item_bar)
+    }
+    return
+}
+
+technical_skill()
